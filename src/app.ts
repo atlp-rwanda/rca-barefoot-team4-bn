11f37247f725c50bd3fe59bc3fab3bb9d7697464
ishgervais <ishimwegervais11@gmail.com>
2023-03-08 13:44:25 +0200

    feat: added the welcoming endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 
 
@@ -9,6 +9,9 @@ app.use(bodyParser.json());
 
 const PORT = 3000;
 
+app.get('/',(req:Request, res:Response) => {
+    res.send("Welcome to Barefoot Nomad APIs")
+})
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
