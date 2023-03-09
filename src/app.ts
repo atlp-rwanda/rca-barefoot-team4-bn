8d0c7817af0d4ca3ffdeaf551fb48a2a49a5e04f
Daniel Dukundane <52023375+Daniel-Dukundane@users.noreply.github.com>
2023-03-09 14:51:56 +0200

    Testing HoundCI

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.get('/',(req:Request, res:Response) => {
     res.send("Welcome to Barefoot Nomad APIs")
 })
 
-app.listen(PORT, () => {fadsf
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
