a2bf83be8f01be07bc21984eea2348f686f5e714
gershomlapaix <nsengiyumv33@gmail.com>
2023-03-09 05:55:11 +0200

    feat: initial eslint configuration
    feat: adding some commands to the eslint file
    
    feat: adding prettierrc for automatic code formatting
    
    feat: adding husky to the project
    
    feat: adding the commands for husky
    
    feat: updating readme file
    
    eslint configuration
    
    bg: fixing linting errors

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# rca-barefoot-team2-bn
+## rca-barefoot-team2-bn
+
+# Linting and formatting commands
+ * `yarn lint` to lint the codes
+ * `yarn format` to format the code to comply with linting rules
+
+# Prettier and husky
+  * `prettier`: drastically improves team consistency by automatically formatting the code
+  * `husky` : will ensure that before committing any work to the project, the code are well linted. This will help to avoid failures in CI builds beforehand.  
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,17 +1,18 @@
-import chai from 'chai';
-import chaiHttp from 'chai-http';
-import app from '../src/app';
+import chai from "chai";
+import chaiHttp from "chai-http";
+import app from "../src/app";
 
 chai.use(chaiHttp);
 const expect = chai.expect;
 
-describe('GET /', () => {
+describe("GET /", () => {
   it('should return "Welcome to Barefoot Nomad APIs"', (done) => {
-    chai.request(app)
-      .get('/')
+    chai
+      .request(app)
+      .get("/")
       .end((err, res) => {
         expect(res.status).to.equal(200);
-        expect(res.text).to.equal('Welcome to Barefoot Nomad APIs');
+        expect(res.text).to.equal("Welcome to Barefoot Nomad APIs");
         done();
       });
   });
