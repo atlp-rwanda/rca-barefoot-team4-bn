d62651537e7772b499e9023d941bb71f727b07d6
Mugisha-isaac <mugishaisaac0508@gmail.com>
2023-03-14 15:04:05 +0200

    fix: remove sqwaggify and its types

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { type Request, type Response } from "express";
 import bodyParser from "body-parser";
-import { Swaggiffy } from "swaggiffy";
-
+import swaggerUi, { type SwaggerUiOptions } from "swagger-ui-express";
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,22 +8,35 @@ app.use(bodyParser.json());
 
 const PORT = 3000;
 
-new Swaggiffy()
-  .setupExpress(app)
-  .swaggiffy()
-  .then(() => {
-    console.log(`swagger api documentation is configured successfully`);
-  })
-  .catch((err) => {
-    console.log(
-      `Error found while configuring swagger api documentation `,
-      err
-    );
-  });
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "RCA BAREFOOT TEam4 Bin Backend APIS",
+    version: "1.0.0",
+    description: "Documented Swagger Apis For RCA-BAREFOOT-TEAM4-BIN Project",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000",
+    },
+  ],
+};
+const options: SwaggerUiOptions = {
+  swaggerOptions: {
+    apis: [
+      {
+        url: "./routes/*.routes.ts",
+      },
+    ],
+  },
+};
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Barefoot Nomad APIs");
 });
+
+app.use("/api-docs", swaggerUi.serve);
+app.get("/api-docs", swaggerUi.setup(swaggerDefinition, options));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
