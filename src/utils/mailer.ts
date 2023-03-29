d12089b4ca782835b62c6a36c0c668b86fc7d8d8
nshimiyeemmy <nshimiyee311@gmail.com>
2023-03-29 21:50:23 +0200

    feat: email delivery service

---
+++ src/utils/mailer.ts
@@ -0,0 +1,16 @@
+import sendGrid from "@sendgrid/mail";
+import { type IEmailMessage } from "index";
+sendGrid.setApiKey(process.env.SENDGRID_API_KEY ?? "");
+
+export async function sendEmail(
+  msg: IEmailMessage
+): Promise<[sendGrid.ClientResponse, object]> {
+  const message = {
+    to: msg.to,
+    from: "barefootnomadd@gmail.com",
+    subject: msg.subject,
+    html: `<div style="font-size:17px;text-align:left;color:#181a19">${msg.message}</div>`,
+  };
+
+  return await sendGrid.send(message);
+}
