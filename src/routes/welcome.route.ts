cf1bd49cb4193bd32daed3e39110053b00e7acba
Iradukunda tamara <tamarameme05@gmail.com>
2023-04-17 11:25:04 +0200

    Revert "feat: changes for ci/cd passing"
    This reverts commit 4799e5c7fc0a1e46284aa315737c9ab650190798.

--- src/routes/welcome.route.ts
+++ src/routes/welcome.route.ts
@@ -1,6 +1,7 @@
 import express, { type Request, type Response } from "express";
 const router = express.Router();
 
+
 router.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Barefoot Nomad APIs");
 });
