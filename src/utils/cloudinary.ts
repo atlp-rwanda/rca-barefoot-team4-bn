0a673b570ed691fba307017b659182781eb9a3ac
gershomlapaix <nsengiyumv33@gmail.com>
2023-05-05 13:22:41 +0200

    fix: convention

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -2,9 +2,9 @@ import {v2 as cloudinary} from 'cloudinary'
 import config from 'config'
 
 cloudinary.config({
-    cloud_name: config.get<string>("cloud_name"),
-    api_key: config.get<string>("api_key"),
-    api_secret: config.get<string>("api_secret")
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
 })
 
 
